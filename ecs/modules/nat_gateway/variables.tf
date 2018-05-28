d0deb6d75599a9afc98bfc126dd1dac53f53d8c0
Abhishek Srivastava <abyshrek@gmail.com>
2018-05-28 09:03:21 +0530

    Removing the old commit to create a new structure for the repository

--- ecs/modules/nat_gateway/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "subnet_ids" {
-  type        = "list"
-  description = "List of subnets where NAT Gateway will be placed"
-}
-
-variable "subnet_count" {
-  description = "Size of the subnet_ids. This needs to be provided because: value of 'count' cannot be computed"
-}
